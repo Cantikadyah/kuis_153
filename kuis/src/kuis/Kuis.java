0958207535966e29e2b1c30a2729ccb5c188e214
Cantikadyah <127312767+Cantikadyah@users.noreply.github.com>
2023-03-24 15:11:20 +0700

    Add files via upload

---
+++ kuis/src/kuis/Kuis.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kuis;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class Kuis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+    }
+    
+}
